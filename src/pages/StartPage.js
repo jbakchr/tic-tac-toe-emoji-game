367998fc581310d0751395b1dd2e41bf4cb20caa
Jonas Bak Phillipson <jbakchr@gmail.com>
2020-04-20 16:01:26 +0200

    Start header fixed

--- src/pages/StartPage.js
+++ src/pages/StartPage.js
@@ -3,18 +3,23 @@ import React from "react";
 import { EmojiGrid } from "../components/Emojis/EmojiGrid";
 
 export const StartPage = ({ emojis, players, gridSize }) => {
-  let header;
-  if (!players[0].emoji) {
-    header = "Please select an emoji player one";
-  } else if (!players[1].emoji) {
-    header = "Please select an emoji player two";
-  } else {
-    header = "Start tha game!";
-  }
+  const renderHeader = () => {
+    let header;
+    if (!players[0].emoji) {
+      header = "Please select an emoji player one";
+    } else if (!players[1].emoji) {
+      header = "Please select an emoji player two";
+    } else {
+      header = "Start tha game!";
+    }
+    return header;
+  };
 
   return (
     <div>
-      <h2 style={{ textAlign: "center" }}>{header}</h2>
+      <h2 style={{ textAlign: "center" }}>{renderHeader()}</h2>
+      <p>Player one emoji: </p>
+      <p>Player two emoji: </p>
       <EmojiGrid emojis={emojis} gridSize={gridSize} />
     </div>
   );
