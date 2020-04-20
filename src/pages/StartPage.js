33e541bec59194a83d478aa55e9601af9c47d0b6
Jonas Bak Phillipson <jbakchr@gmail.com>
2020-04-20 20:01:53 +0200

    Player display added

--- src/pages/StartPage.js
+++ src/pages/StartPage.js
@@ -1,4 +1,5 @@
 import React from "react";
+import { Button } from "@material-ui/core";
 
 import { EmojiGrid } from "../components/Emojis/EmojiGrid";
 
@@ -15,16 +16,43 @@ export const StartPage = ({ emojis, players, gridSize, onEmojiClick }) => {
     return header;
   };
 
+  const renderStartGameButton = () => {
+    let disabled = false;
+    if (!players[0].emoji || !players[1].emoji) {
+      disabled = true;
+    }
+
+    return (
+      <div
+        style={{
+          display: "flex",
+          justifyContent: "center",
+          alignItems: "center",
+          marginTop: 20,
+        }}
+      >
+        <Button variant="contained" color="primary" disabled={disabled}>
+          Start Game
+        </Button>
+      </div>
+    );
+  };
+
   return (
     <div>
-      <h2 style={{ textAlign: "center" }}>{renderHeader()}</h2>
-      <p>Player one emoji: {players[0].emoji} </p>
-      <p>Player two emoji: {players[1].emoji} </p>
+      <h2 style={{ textAlign: "center", marginBottom: 30 }}>
+        {renderHeader()}
+      </h2>
+      <div style={{ marginBottom: 30 }}>
+        <p>Player one emoji: {players[0].emoji} </p>
+        <p>Player two emoji: {players[1].emoji} </p>
+      </div>
       <EmojiGrid
         emojis={emojis}
         gridSize={gridSize}
         onEmojiClick={onEmojiClick}
       />
+      {renderStartGameButton()}
     </div>
   );
 };
